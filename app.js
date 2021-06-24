cb96505cba5bd6481dfb80c80b2df3600cffd50c
Fairfaxx <fede.lemaire@gmail.com>
2021-06-23 21:12:32 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Starting');
+
+setTimeout(() => {
+  console.log('Waiting for the server 😂');
+}, 5000);
+
+setTimeout(() => {
+  console.log('0 seconds');
+}, 0)
+
+console.log('Finished');
